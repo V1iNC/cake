4787e9c60430ea334dbbd80cd492c18359ddb62e
V1iNC <113884588+V1iNC@users.noreply.github.com>
2025-04-26 19:31:23 +0300

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,1 +1,11 @@
+import React from 'react';
 
+const Footer = () => (
+  <footer className="footer">
+    <div className="container">
+      <p>© {new Date().getFullYear()} Сладкий Чек — Все права защищены</p>
+    </div>
+  </footer>
+);
+
+export default Footer;
