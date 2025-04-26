9a0c40a91cfdaf109f55e83c5e60b27c93535353
V1iNC <113884588+V1iNC@users.noreply.github.com>
2025-04-26 19:30:27 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,1 +1,27 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+import Catalog from './pages/Catalog';
+import ProductDetails from './pages/ProductDetails';
+import Contact from './pages/Contact';
 
+function App() {
+  return (
+    <Router>
+      <Header />
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/catalog" element={<Catalog />} />
+          <Route path="/product/:id" element={<ProductDetails />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </main>
+      <Footer />
+    </Router>
+  );
+}
+
+export default App;
