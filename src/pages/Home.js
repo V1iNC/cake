716330ac84a5300750b87e1b69852eeab247c5ae
V1iNC <113884588+V1iNC@users.noreply.github.com>
2025-04-26 19:34:46 +0300

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,1 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
 
+const Home = () => (
+  <section className="home">
+    <div className="container home-content">
+      <h2>Индивидуальные торты на заказ в Москве</h2>
+      <p>Создаём сладкие шедевры на любое событие: свадьбы, дни рождения, корпоративы и просто к чаю.</p>
+      <Link to="/catalog" className="btn">Перейти в каталог</Link>
+    </div>
+  </section>
+);
+
+export default Home;
