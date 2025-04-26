024b1fecb16d0edaeb31dc8a77a6f2e7a31ef1ee
V1iNC <113884588+V1iNC@users.noreply.github.com>
2025-04-26 19:30:59 +0300

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,1 +1,17 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
 
+const Header = () => (
+  <header className="header">
+    <div className="container">
+      <h1 className="logo">Сладкий Чек</h1>
+      <nav className="nav">
+        <Link to="/">Главная</Link>
+        <Link to="/catalog">Каталог</Link>
+        <Link to="/contact">Контакты</Link>
+      </nav>
+    </div>
+  </header>
+);
+
+export default Header;
