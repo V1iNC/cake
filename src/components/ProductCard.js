f1285665f4a4dafaad6a24404dcc7f9c678ff1af
V1iNC <113884588+V1iNC@users.noreply.github.com>
2025-04-26 19:32:13 +0300

    Update ProductCard.js

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,1 +1,13 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
 
+const ProductCard = ({ product }) => (
+  <div className="product-card">
+    <img src={product.image} alt={product.title} className="product-image" />
+    <h3>{product.title}</h3>
+    <p>{product.price} ₽</p>
+    <Link to={/product/${product.id}} className="btn">Подробнее</Link>
+  </div>
+);
+
+export default ProductCard;
