c9b64c50cf0cbe92ac16cf9431ef84b37a9add63
V1iNC <113884588+V1iNC@users.noreply.github.com>
2025-04-28 17:19:26 +0300

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,11 +4,17 @@ import { Link } from 'react-router-dom';
 const Home = () => (
   <section className="home">
     <div className="container home-content">
-      <h2>Индивидуальные торты на заказ в Москве</h2>
+      <h2>Индивидуальные торты на заказ в Воронеже</h2>
       <p>Создаём сладкие шедевры на любое событие: свадьбы, дни рождения, корпоративы и просто к чаю.</p>
       <Link to="/catalog" className="btn">Перейти в каталог</Link>
     </div>
   </section>
 );
+<section className="page home">
+  <div className="container" data-aos="fade-up">
+    <h1>Добро пожаловать в Sweet Treats!</h1>
+    <p>Лучшие торты и пирожные в Воронеже!</p>
+  </div>
+</section>
 
 export default Home;
