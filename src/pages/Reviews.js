c123c0389349f10bb0243bc10b73bcf73a554185
V1iNC <113884588+V1iNC@users.noreply.github.com>
2025-04-28 16:11:32 +0300

    Create Reviews.js

---
+++ src/pages/Reviews.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Reviews = () => {
+  return (
+    <section className="page">
+      <div className="container">
+        <h1>Отзывы</h1>
+        <p>Наши клиенты делятся своими впечатлениями о наших сладостях!</p>
+      </div>
+    </section>
+  );
+};
+
+export default Reviews;
