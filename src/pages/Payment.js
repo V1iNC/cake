9a7ec8a2caa41ef6c43e0be67e998a3015329b0f
V1iNC <113884588+V1iNC@users.noreply.github.com>
2025-04-28 16:07:02 +0300

    Create Payment.js

---
+++ src/pages/Payment.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Payment = () => {
+  return (
+    <section className="page">
+      <div className="container">
+        <h1>Оплата</h1>
+        <p>Здесь будет информация о способах оплаты ваших заказов.</p>
+      </div>
+    </section>
+  );
+};
+
+export default Payment;
