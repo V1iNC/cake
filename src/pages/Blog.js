8636c5e7d3ba4cc56fddcfeb1f20f4e6949bdda8
V1iNC <113884588+V1iNC@users.noreply.github.com>
2025-04-28 16:09:55 +0300

    Create Blog.js

---
+++ src/pages/Blog.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Blog = () => {
+  return (
+    <section className="page">
+      <div className="container">
+        <h1>Блог</h1>
+        <p>Читайте полезные статьи о наших тортах, выпечке и новостях компании!</p>
+      </div>
+    </section>
+  );
