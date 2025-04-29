1f8fb88acae2b35b7f247ab580825b0c9692c932
V1iNC <113884588+V1iNC@users.noreply.github.com>
2025-04-29 17:19:30 +0300

    Update Reviews.js

--- src/pages/Reviews.js
+++ src/pages/Reviews.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Reviews = () => {
   return (
-    <section className="page">
-      <div className="container">
+    <section className="page reviews" id="reviews">
+      <div className="container" data-aos="fade-up">
         <h1>Отзывы</h1>
         <p>Наши клиенты делятся своими впечатлениями о наших сладостях!</p>
       </div>
