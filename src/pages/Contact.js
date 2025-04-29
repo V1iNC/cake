bd74643d4c89c002f7a4b7ab9dcb88aa677f5668
V1iNC <113884588+V1iNC@users.noreply.github.com>
2025-04-29 17:12:46 +0300

    Update Contact.js

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,10 +1,11 @@
 import React from 'react';
 
 const Contact = () => (
-  <section className="contact">
-    <div className="container">
-      <h2>Оставьте заявку на заказ</h2>
-      <form className="contact-form">
+  <section className="page contact" id="contact">
+    <div className="container" data-aos="fade-up">
+      <h1>Связаться с нами</h1>
+      <p>Заполните форму, и мы вам ответим!</p>
+       <form className="contact-form">
         <label>
           Ваше имя:
           <input type="text" name="name" required />
