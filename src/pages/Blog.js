f1e87b5cfa34e9d11d74abbb89dcd3d33a8826b0
V1iNC <113884588+V1iNC@users.noreply.github.com>
2025-04-29 17:16:52 +0300

    Update Blog.js

--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 const Blog = () => {
   return (
-    <section className="page">
-      <div className="container">
+    <section className="page blog" id="blog">
+      <div className="container" data-aos="fade-up">
         <h1>Блог</h1>
-        <p>Читайте полезные статьи о наших тортах, выпечке и новостях компании!</p>
+            <p>Читайте полезные статьи о наших тортах, выпечке и новостях компании!</p>
       </div>
     </section>
   );
